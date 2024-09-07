dd9fe50ff28bd6f7112731b5f856f0a8a8d12992
koech victor <kipkiruiv408@gmail.com>
2024-09-07 14:13:49 +0000

    update customer inialization

---
+++ index.py
@@ -0,0 +1,14 @@
+class Customer:
+    def __init__(self, name):
+        self.name = name
+
+    @property
+    def name(self):
+        return self._name
+
+    @name.setter
+    def name(self, value):
+        if isinstance(value, str) and 1 <= len(value) <= 15:
+            self._name = value
+        else:
+            raise ValueError("Name must be a string between 1 and 15 characters.")
